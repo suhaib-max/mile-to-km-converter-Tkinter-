477e47365230757734c646afa07483962828555f
suhaib <suhaibvbm@gmail.com>
2023-11-07 15:12:25 +0530

    created GUI requirements in screen window

--- mile_to_Km.py
+++ mile_to_Km.py
@@ -3,5 +3,18 @@ from tkinter import *
 window = Tk()
 window.title("Miles to Kilometer Converter")
 
+Miles_input = Entry()
+
+Miles_label = Label(text="Miles")
+
+is_equal_label = Label(text="is equal to")
+
+kilometer_result_label = Label(text=0)
+
+kilometer_label = Label(text="Km")
+
+calculate_button = Button(text="Calculate")
+
+
 
 window.mainloop()
